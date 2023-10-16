3090adfef18170897d8d6312a0f77f886c827fbe
Ba5t3t <lovely09lucky@gmail.com>
2023-10-16 17:34:32 +0300

    update date in moment js

--- src/components/Weekday/Weekday.jsx
+++ src/components/Weekday/Weekday.jsx
@@ -1,17 +1,23 @@
 import React from "react";
 import { DateContainer, DateText } from "./StyledWeekday";
+import moment from "moment";
 
 export const Weekday = () => {
-  const weekday = new Date().toLocaleDateString("ru-RU", { weekday: "long" });
-  const month = new Date()
+  //const weekday = new Date().toLocaleDateString("ru-RU", { weekday: "long" });
+  /*  const month = new Date()
     .toLocaleDateString("ru-RU", { month: "short" })
-    .slice(0, -1);
-  const date = new Date();
+    .slice(0, -1) */
 
-  const year = date.getFullYear();
+  const weekday = moment().format("dddd");
+  const month = moment().format("MMMM");
+  const year = moment().format("YYYY");
+  const date = moment().format("D");
+  //const date = new Date();
+
+  //const year = date.getFullYear();
   //const month = String(date.getMonth()).padStart(2, "0");
-  const day = String(date.getDate()).padStart(2, "0");
-  const currentDate = `${day} ${month} ${year}`;
+  //const day = String(date.getDate()).padStart(2, "0");
+  const currentDate = `${date} ${month} ${year}`;
 
   //console.log(date);
 
